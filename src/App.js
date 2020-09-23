007698201405c66c34211691d677a5959b1cf9a7
Md Hidayat Rasool <mdhidayat03@hotmail.com>
2020-09-23 22:48:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+import Header from "./Header";
+import MemeGenerator from "./MemeGenerator";
+
+function App() {
+  return (
+    <div>
+            <Header />
+            <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
